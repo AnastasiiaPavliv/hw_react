c2795fc8a3b75b2bfba7b2b3ad3c01da090ff7e9
AnastasiiaPavliv <pavlivnastia@gmail.com>
2023-01-22 22:00:28 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {useState} from "react";
+import {Users} from "./Componets/Users/Users";
+import {Posts} from "./Componets/Posts/Posts";
 
 const App = () => {
+    const [userId, setUserId]=useState([]);
   return (
       <div>
-
+<Users setUserId={setUserId}/>
+<hr/>
+          {userId && <Posts userId={userId}/>}
       </div>
   );
 };
---
+++ src/services/userService.js
@@ -0,0 +1,8 @@
+import {axiosService} from "./axiosService";
+
+const userService={
+    getAll:()=>axiosService.get('/users')
+}
+export {
+    userService
+}
